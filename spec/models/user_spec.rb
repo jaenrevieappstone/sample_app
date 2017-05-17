b7f2f69500ee919adaf9398dafab7d7ace074e64
Jaenrevie <jaenrevie.alo@appstone.com>
2017-05-17 15:29:43 +0800

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,7 +30,6 @@ describe User do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
-
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
@@ -57,9 +56,19 @@ describe User do
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
-
     it { should_not be_valid }
   end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
 	  before do
 	    @user = User.new(name: "Example User", email: "user@example.com",
